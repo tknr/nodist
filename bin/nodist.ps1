5ac4ce1fae71c60e315d1f5c1a1f943f1df13d6a
tknr <geishagirlskoume@gmail.com>
2023-12-15 18:20:10 +0900

    Update nodist.ps1

--- bin/nodist.ps1
+++ bin/nodist.ps1
@@ -1,17 +1,22 @@
 param($cmd,$ver,[Switch]$v)
+
+[System.Environment]::SetEnvironmentVariable("NODE_TLS_REJECT_UNAUTHORIZED", 0, "User")
+[System.Environment]::SetEnvironmentVariable("NODIST_PREFIX", $env:HOMEPATH + '\scoop\apps\nodist-portable\current', "User")
+$env:NODIST_PREFIX = $env:HOMEPATH + '\scoop\apps\nodist-portable\current'
+
 if ($cmd -eq "use" -or $cmd -eq "env") {
-  nodist.cmd add $ver
-  $env:NODIST_NODE_VERSION = $ver
-  echo $ver
+ 	nodist.cmd add $ver
+ 	$env:NODIST_NODE_VERSION = $ver
+ 	echo $ver
 }
 elseif ($cmd -eq "npm" -and $ver -eq "env") {
-  nodist.cmd npm add $args[0]
-  $env:NODIST_NPM_VERSION = $args[0]
-  echo $args[0]
+ 	nodist.cmd npm add $args[0]
+ 	$env:NODIST_NPM_VERSION = $args[0]
+ 	echo $args[0]
 }
 elseif ($v){
-  nodist.cmd -v
+ 	nodist.cmd -v
 }
 else {
-  nodist.cmd $cmd $ver ([string]$args)
+ 	nodist.cmd $cmd $ver ([string]$args)
 }
